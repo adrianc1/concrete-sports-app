b0786e9e9e40e1591f0e3173f0333caf1a3ec461
adriansito <adriancrosby02@gmail.com>
2025-02-22 23:38:02 -0800

    updated sidenav postition and header. routed links

--- src/components/sideNavBar/SideNavBar.jsx
+++ src/components/sideNavBar/SideNavBar.jsx
@@ -1,19 +1,18 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import ListGroup from 'react-bootstrap/ListGroup';
 import './sideNavBar.css';
+import { Link } from 'react-router';
 
 function SideNavBar({ show, logo }) {
 	return (
 		<ListGroup className={show ? 'side-nav active' : 'side-nav'}>
 			<img src={logo} alt="Logo" className="logo" />
-			<ListGroup.Item action href="https://www.concrete.k12.wa.us/sports/">
-				Home
+
+			<ListGroup.Item>
+				<Link to="/">Home</Link>
 			</ListGroup.Item>
-			<ListGroup.Item
-				action
-				href="https://concretehslions.com/teams/3446004/basketball/varsity/schedule"
-			>
-				Schedule
+
+			<ListGroup.Item>
+				<Link to="/SchedulePage">Schedule</Link>
 			</ListGroup.Item>
 
 			<ListGroup.Item
