6a58e4e5f63a1015c77730ed333464a0d525f529
adriansito <adriancrosby02@gmail.com>
2025-03-29 23:05:12 -0700

    added coach connect page

--- src/components/mainPage/Contact.jsx
+++ src/components/mainPage/Contact.jsx
@@ -2,9 +2,43 @@ import { useState } from 'react';
 import Header from '../header/Header';
 import logo from '../../assets/Concrete-Logo.png';
 import SideNavBar from '../sideNavBar/SideNavBar';
+import './contact.css';
 function Contact() {
 	const [showNav, setShowNav] = useState(false);
 	const [showExitIcon, setShowExitIcon] = useState(false);
+
+	const coachList = [
+		{
+			team: 'Boys Basketball',
+			coachName: 'Danny Crosby',
+			coachEmail: 'coachdanny@notrealemail.com',
+			coachPhone: '360-555-1234',
+		},
+		{
+			team: 'Girls Basketball',
+			coachName: 'Head Coach',
+			coachEmail: 'girlsbball@notrealemail.com',
+			coachPhone: '360-555-0123',
+		},
+		{
+			team: 'Softball',
+			coachName: 'Head Coach',
+			coachEmail: 'softball@notrealemail.com',
+			coachPhone: '360-555-0125',
+		},
+		{
+			team: 'Baseball',
+			coachName: 'Head Coach',
+			coachEmail: 'baseball@notrealemail.com',
+			coachPhone: '360-555-0129',
+		},
+		{
+			team: 'Wrestling',
+			coachName: 'Head Coach',
+			coachEmail: 'wrestling@notrealemail.com',
+			coachPhone: '360-555-0127',
+		},
+	];
 	return (
 		<div>
 			<Header
@@ -14,18 +48,26 @@ function Contact() {
 				display={showExitIcon}
 			/>
 			<SideNavBar show={showNav} logo={logo} />
-			<iframe
-				src="https://concretehslions.com/events"
-				title="Events Calendar"
-				style={{
-					position: 'fixed',
-					top: '1rem',
-					left: 0,
-					width: '100vw',
-					height: '100vh',
-					border: 'none',
-				}}
-			></iframe>
+
+			<div className="contact-page">
+				<div className="page-title">Coach Connect</div>
+
+				<ul className="coach-list">
+					{coachList.map((coach, index) => {
+						console.log(coach);
+						return (
+							<li className="coach-item" key={index}>
+								<span className="coach-title"> {coach.team}</span>
+								<span className="coach-name">
+									Head Coach: {coach.coachName}
+								</span>
+								<span className="coach-email">Email: {coach.coachEmail}</span>
+								<span className="coach-phone">Phone: {coach.coachPhone}</span>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 }
