8740159a73e0662aee2a30ca29b770a7ce102cbe
adriansito <adriancrosby02@gmail.com>
2025-08-04 21:27:50 -0700

    udpated football and varsity season start dates and ties

--- src/components/homePage/Updates.jsx
+++ src/components/homePage/Updates.jsx
@@ -31,57 +31,17 @@ function Updates() {
 				<li>
 					<span className="event-info">
 						Football Season Begins
-						<span className="start-date">09/11/25</span>
+						<span className="start-date">September 5, 2025</span>
 					</span>
+					<span className="start-time">7:00pm</span>
 				</li>
 
 				<li>
 					<span className="event-info">
 						Girls Volleyball Season Begins
-						<span className="start-date">09/18/25</span>
+						<span className="start-date">September 8, 2025</span>
 					</span>
-				</li>
-
-				<li>
-					<span className="event-info">
-						Boys Basketball Season Begins
-						<span className="start-date">11/10/25</span>
-					</span>
-				</li>
-
-				<li>
-					<span className="event-info">
-						Girls Basketball Season Begins
-						<span className="start-date">11/10/25</span>
-					</span>
-				</li>
-
-				<li>
-					<span className="event-info">
-						Wrestling Season Begins
-						<span className="start-date">11/22/25</span>
-					</span>
-				</li>
-
-				<li>
-					<span className="event-info">
-						Baseball Season Begins
-						<span className="start-date">03/25/26</span>
-					</span>
-				</li>
-
-				<li>
-					<span className="event-info">
-						Softball Season Begins
-						<span className="start-date">03/24/26</span>
-					</span>
-				</li>
-
-				<li>
-					<span className="event-info">
-						Track Season Begins
-						<span className="start-date">02/22/26</span>
-					</span>
+					<span className="start-time">4:00pm</span>
 				</li>
 			</ul>
 		</div>
