ade83532251e65d7c515519d667cc26cb76ab65a
adriansito <adriancrosby02@gmail.com>
2025-08-07 20:08:36 -0700

    added annies banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,14 +4,16 @@ import actOneLogo from '../../assets/actonelogo.jpg';
 import kayak from '../../assets/kayak2.png';
 import uptownAuto from '../../assets/uptown2.png';
 import perks from '../../assets/perks2.png';
+import annies from '../../assets/anniespizza.png';
 import './banner.css';
 function Banner() {
 	const slides = [
 		{ url: welcomeBanner },
-		{ url: actOneLogo },
 		{ url: kayak },
 		{ url: uptownAuto },
 		{ url: perks },
+		{ url: annies },
+		{ url: actOneLogo },
 	];
 
 	return (
