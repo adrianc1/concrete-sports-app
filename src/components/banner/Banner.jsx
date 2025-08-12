a129e2d142c0666b8d8425a0027bf35800cb177e
adriansito <adriancrosby02@gmail.com>
2025-08-11 21:21:28 -0700

    added 3 new banners for cascade burgers

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,10 +5,17 @@ import uptownAuto from '../../assets/uptown2.png';
 import perks from '../../assets/perks5.png';
 import annies from '../../assets/anniespizza.png';
 import cascadeBurgers from '../../assets/cascadeburgerbanner.png';
+import cascade1 from '../../assets/cascade1.png';
+import cascade2 from '../../assets/cascade2.png';
+import cascade3 from '../../assets/cascade3.png';
+
 import './banner.css';
 function Banner() {
 	const slides = [
 		{ url: welcomeBanner },
+		{ url: cascade1 },
+		{ url: cascade2 },
+		{ url: cascade3 },
 		{ url: cascadeBurgers },
 		{ url: annies },
 		{ url: kayak },
