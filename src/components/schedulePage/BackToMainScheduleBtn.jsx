ed1080be55fa20177cc8a169369969611ab2f9d1
adriansito <adriancrosby02@gmail.com>
2025-08-22 17:16:18 -0700

    fixed css import statement for back to schedule button

--- src/components/schedulePage/BackToMainScheduleBtn.jsx
+++ src/components/schedulePage/BackToMainScheduleBtn.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router';
-import './ScheduleBtn.css';
+import './scheduleBtn.css';
 export default function BackToMainScheduleBtn() {
 	return (
 		<Link to="/Schedules">
