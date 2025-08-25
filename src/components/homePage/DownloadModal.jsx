2b0e132785675eb7835e8c0d0a4f16010b1fae13
adriansito <adriancrosby02@gmail.com>
2025-08-24 22:46:10 -0700

    added images to the get app modal step by step instructions

--- src/components/homePage/DownloadModal.jsx
+++ src/components/homePage/DownloadModal.jsx
@@ -1,4 +1,7 @@
 import '../homePage/homePageStyles/modal.css';
+import step1 from '../../assets/step1.png';
+import step2 from '../../assets/step2.png';
+
 export default function DownloadModal({ setShowModal }) {
 	function closeModal() {
 		setShowModal(false);
@@ -8,10 +11,18 @@ export default function DownloadModal({ setShowModal }) {
 			<span onClick={closeModal}>X</span>
 			<h2>Concrete Sports App</h2>
 			<p>Get the app in 5 seconds. No download required.</p>
-			<ul>
-				<li>1. Tap the share button on the bottom of the screen.</li>
-				<li>2. Tap "Add To Home Screen" button.</li>
-				<li>3. Tap the "Add" button in top right corner.</li>
+			<ul className="how-to">
+				<li>
+					<p>1. Tap the share button on the bottom of the screen.</p>
+					<img src={step1} alt="" />
+				</li>
+				<li>
+					<p>2. Tap "Add To Home Screen" button.</p>
+					<img src={step2} alt="" />
+				</li>
+				<li>
+					<p>3. Tap the "Add" button in top right corner.</p>
+				</li>
 			</ul>
 		</div>
 	);
