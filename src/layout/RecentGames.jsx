0367c73f35536fea5e7be74973856ee04e849964
adriansito <adriancrosby02@gmail.com>
2025-10-30 09:57:44 -0700

    updated volleyball scores and recent games. Recent container width decreased to 95%

--- src/layout/RecentGames.jsx
+++ src/layout/RecentGames.jsx
@@ -6,7 +6,45 @@ const RecentGames = () => {
 
 			<div className="recent-container">
 				<div className="recent-games">
-					<div className="game-card game-4">
+					<div className="game-card game-1">
+						<div className="game-info-card">
+							<div className="game-date">WED Oct 29</div>
+							<div className="sport-name">Varsity Volleyball</div>
+						</div>
+						<div className="score-container">
+							<div className="team-container">
+								<div className="team-name">Concrete</div>
+								<div className="team-score">3</div>
+							</div>
+							<div className="team-container">
+								<div className="team-name">Evangel Classical</div>
+								<div className="team-score">0</div>
+							</div>
+							<Link to="/volleyballSchedule">
+								<button className="see-schedule">Schedule</button>
+							</Link>
+						</div>
+					</div>
+					<div className="game-card game-2">
+						<div className="game-info-card">
+							<div className="game-date">WED Oct 29</div>
+							<div className="sport-name">Varsity Volleyball</div>
+						</div>
+						<div className="score-container">
+							<div className="team-container">
+								<div className="team-name">Concrete</div>
+								<div className="team-score">0</div>
+							</div>
+							<div className="team-container">
+								<div className="team-name">Darrington</div>
+								<div className="team-score">3</div>
+							</div>
+							<Link to="/volleyballSchedule">
+								<button className="see-schedule">Schedule</button>
+							</Link>
+						</div>
+					</div>
+					<div className="game-card game-3">
 						<div className="game-info-card">
 							<div className="game-date">MON Oct 27</div>
 							<div className="sport-name">Varsity Volleyball</div>
@@ -25,7 +63,7 @@ const RecentGames = () => {
 							</Link>
 						</div>
 					</div>
-					<div className="game-card game-1">
+					<div className="game-card game-4">
 						<div className="game-info-card">
 							<div className="game-date">SAT Oct 25</div>
 							<div className="sport-name">Varsity Football</div>
@@ -40,44 +78,6 @@ const RecentGames = () => {
 								<div className="team-score">52</div>
 							</div>
 							<Link to="/footballSchedule">
-								<button className="see-schedule">Schedule</button>
-							</Link>
-						</div>
-					</div>
-					<div className="game-card game-2">
-						<div className="game-info-card">
-							<div className="game-date">FRI Oct 24</div>
-							<div className="sport-name">Varsity Volleyball</div>
-						</div>
-						<div className="score-container">
-							<div className="team-container">
-								<div className="team-name">Concrete</div>
-								<div className="team-score">3 </div>
-							</div>
-							<div className="team-container">
-								<div className="team-name">Evangel Classical</div>
-								<div className="team-score">0</div>
-							</div>
-							<Link to="/volleyballSchedule">
-								<button className="see-schedule">Schedule</button>
-							</Link>
-						</div>
-					</div>
-					<div className="game-card game-3">
-						<div className="game-info-card">
-							<div className="game-date">THU Oct 23</div>
-							<div className="sport-name">Varsity Volleyball</div>
-						</div>
-						<div className="score-container">
-							<div className="team-container">
-								<div className="team-name">Concrete</div>
-								<div className="team-score">0</div>
-							</div>
-							<div className="team-container">
-								<div className="team-name">Coupeville</div>
-								<div className="team-score">3</div>
-							</div>
-							<Link to="/volleyballSchedule">
 								<button className="see-schedule">Schedule</button>
 							</Link>
 						</div>
