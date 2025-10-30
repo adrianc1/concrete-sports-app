f1b6cf3ecc997d15a968006ad3c1db7f182b55e9
adriansito <adriancrosby02@gmail.com>
2025-10-30 15:58:46 -0700

    updaeted api.js file into the utils folder and calling fetchSchedule from frontend

--- src/components/schedulePage/VolleyballSchedule.jsx
+++ src/components/schedulePage/VolleyballSchedule.jsx
@@ -1,8 +1,37 @@
-import games from '../../utils/webScraper/volleyballGames.json';
+import { useState, useEffect } from 'react';
 import logo from '../../assets/Concrete-Logo.png';
 import SchedulePage from './SchedulePage';
+import { fetchSchedules } from '../../utils/api.js';
 import './schedulePage.css';
 
 export default function VolleyballSchedule() {
+	const [games, setGames] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		async function loadGames() {
+			try {
+				setLoading(true);
+				const data = await fetchSchedules('volleyball');
+				setGames(data);
+			} catch (err) {
+				setError(err);
+				console.log('error loading up the schedules');
+			} finally {
+				setLoading(false);
+			}
+		}
+		loadGames();
+	}, []);
+
+	if (loading) {
+		return <div>Loading volleyball schedule...</div>;
+	}
+
+	if (error) {
+		return <div>Error loading schedule: {error}</div>;
+	}
+
 	return <SchedulePage sportName="Volleyball" games={games} logo={logo} />;
 }
