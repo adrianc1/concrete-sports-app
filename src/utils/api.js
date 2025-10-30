dfb15581a9e5504adcd4b6da7292c33a2fcdbde4
adriansito <adriancrosby02@gmail.com>
2025-10-30 15:58:46 -0700

    updaeted api.js file into the utils folder and calling fetchSchedule from frontend

---
+++ src/utils/api.js
@@ -0,0 +1,15 @@
+const API_URL = 'http://localhost:3030';
+
+export async function fetchSchedules(sport) {
+	try {
+		const response = await fetch(`${API_URL}/data/${sport}`);
+
+		if (!response.ok) {
+			throw new Error(`Failed to fetch ${sport} schedule`);
+		}
+		return await response.json();
+	} catch (error) {
+		console.error('Error fetching schedules:', error);
+		throw error;
+	}
+}
