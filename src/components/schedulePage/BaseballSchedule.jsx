84bf99d037474cf7be4b811fe0e928bb52c81d40
adriansito <adriancrosby02@gmail.com>
2025-10-30 20:35:26 -0700

    created useSchedules hook to make api call to db

--- src/components/schedulePage/BaseballSchedule.jsx
+++ src/components/schedulePage/BaseballSchedule.jsx
@@ -1,8 +1,12 @@
-import games from '../../utils/webScraper/baseballGames.json';
 import logo from '../../assets/Concrete-Logo.png';
 import SchedulePage from './SchedulePage';
+import { useSchedules } from '../../hooks/useSchedules';
 import './schedulePage.css';
 
 export default function BaseballSchedule() {
+	const { games, loading } = useSchedules('baseball');
+	if (loading) {
+		return <div>Loading baseball schedule...</div>;
+	}
 	return <SchedulePage sportName="Baseball" games={games} logo={logo} />;
 }
--- src/components/schedulePage/BoysBasketballSchedule.jsx
+++ src/components/schedulePage/BoysBasketballSchedule.jsx
@@ -1,8 +1,12 @@
-import games from '../../utils/webScraper/boysbballgames.json';
+import { useSchedules } from '../../hooks/useSchedules';
 import logo from '../../assets/Concrete-Logo.png';
 import SchedulePage from './SchedulePage';
 import './schedulePage.css';
 
 export default function BoysBasketballSchedule() {
+	const { games, loading } = useSchedules('boys-basketball');
+	if (loading) {
+		return <div>Loading boys basketball schedule...</div>;
+	}
 	return <SchedulePage sportName="Boys Basketball" games={games} logo={logo} />;
 }
