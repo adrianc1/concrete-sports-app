5a5a625ae3d06cd1cfa44892f0c0e752776aefb2
adriansito <adriancrosby02@gmail.com>
2025-10-30 20:35:26 -0700

    created useSchedules hook to make api call to db

--- src/components/schedulePage/VolleyballSchedule.jsx
+++ src/components/schedulePage/VolleyballSchedule.jsx
@@ -1,36 +1,13 @@
-import { useState, useEffect } from 'react';
 import logo from '../../assets/Concrete-Logo.png';
 import SchedulePage from './SchedulePage';
-import { fetchSchedules } from '../../utils/api.js';
 import './schedulePage.css';
+import { useSchedules } from '../../hooks/useSchedules.js';
 
 export default function VolleyballSchedule() {
-	const [games, setGames] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
-
-	useEffect(() => {
-		async function loadGames() {
-			try {
-				setLoading(true);
-				const data = await fetchSchedules('volleyball');
-				setGames(data);
-			} catch (err) {
-				setError(err);
-				console.log('error loading up the schedules');
-			} finally {
-				setLoading(false);
-			}
-		}
-		loadGames();
-	}, []);
+	const { games, loading } = useSchedules('volleyball');
 
 	if (loading) {
 		return <div>Loading volleyball schedule...</div>;
-	}
-
-	if (error) {
-		return <div>Error loading schedule: {error}</div>;
 	}
 
 	return <SchedulePage sportName="Volleyball" games={games} logo={logo} />;
