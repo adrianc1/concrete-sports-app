9985c0a79ee07eb001852e00ffe1cde4987d6dce
adriansito <adriancrosby02@gmail.com>
2025-12-15 20:51:30 -0800

    updated bottom nav icon colors to white. Increased padding on footer on body of main page

--- src/components/homePage/BottomNav.jsx
+++ src/components/homePage/BottomNav.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import { RiContactsLine } from 'react-icons/ri';
 import { RiMegaphoneLine } from 'react-icons/ri';
 import { PiUsers } from 'react-icons/pi';
@@ -11,34 +12,34 @@ const BottomNav = () => {
 		<div className="bottom-nav-container">
 			<ul className="bottom-nav-list">
 				<li className="bottom-nav-list-item">
-					<a href="#" className="nav-link">
+					<Link to="/schedules" className="nav-link">
 						<Calendar />
 						<span>Schedules</span>
-					</a>
+					</Link>
 				</li>
 				<li className="bottom-nav-list-item">
-					<a href="#" className="nav-link">
+					<Link to="/sponsors" className="nav-link">
 						<MdOutlineAddBusiness />
 						<span>Sponsors</span>
-					</a>
+					</Link>
 				</li>
 				<li className="bottom-nav-list-item">
-					<a href="#" className="nav-link">
+					<Link to="/contact" className="nav-link">
 						<RiContactsLine />
 						<span>Coaches</span>
-					</a>
+					</Link>
 				</li>
 				<li className="bottom-nav-list-item">
-					<a href="#" className="nav-link">
+					<Link to="playersOfTheMonth" className="nav-link">
 						<PiUsers />
 						<span>Spotlight</span>
-					</a>
+					</Link>
 				</li>
 				<li className="bottom-nav-list-item">
-					<a href="#" className="nav-link">
+					<Link to="boosters" className="nav-link">
 						<GiLion />
 						<span>Booster</span>
-					</a>
+					</Link>
 				</li>
 			</ul>
 		</div>
