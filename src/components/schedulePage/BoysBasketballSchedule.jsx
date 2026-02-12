6e84d96661ca7625cf1a0917063d2f3fedee1ea6
adriansito <adriancrosby02@gmail.com>
2026-02-11 21:27:04 -0800

    added sport record to each sport schedule

--- src/components/schedulePage/BoysBasketballSchedule.jsx
+++ src/components/schedulePage/BoysBasketballSchedule.jsx
@@ -5,8 +5,28 @@ import './schedulePage.css';
 
 export default function BoysBasketballSchedule() {
 	const { games, loading } = useSchedules('boys-basketball');
+	let wins = 0;
+	let loss = 0;
+	let record;
+	games.map((game) => {
+		console.log(game.result.includes('W'));
+
+		if (game.result.includes('W')) {
+			wins += 1;
+		} else if (game.result.includes('L')) {
+			loss += 1;
+		}
+	});
+	record = `${wins} - ${loss}`;
 	if (loading) {
 		return <div>Loading boys basketball schedule...</div>;
 	}
-	return <SchedulePage sportName="Boys Basketball" games={games} logo={logo} />;
+	return (
+		<SchedulePage
+			sportName="Boys Basketball"
+			games={games}
+			logo={logo}
+			record={record}
+		/>
+	);
 }
--- src/components/schedulePage/SoftballSchedule.jsx
+++ src/components/schedulePage/SoftballSchedule.jsx
@@ -5,8 +5,28 @@ import './schedulePage.css';
 
 export default function SoftballSchedule() {
 	const { games, loading } = useSchedules('softball');
+	let wins = 0;
+	let loss = 0;
+	let record;
+	games.map((game) => {
+		console.log(game.result.includes('W'));
+
+		if (game.result.includes('W')) {
+			wins += 1;
+		} else if (game.result.includes('L')) {
+			loss += 1;
+		}
+	});
+	record = `${wins} - ${loss}`;
 	if (loading) {
 		return <div>Loading softball schedule...</div>;
 	}
-	return <SchedulePage sportName="Softball" games={games} logo={logo} />;
+	return (
+		<SchedulePage
+			sportName="Softball"
+			games={games}
+			logo={logo}
+			record={record}
+		/>
+	);
 }
