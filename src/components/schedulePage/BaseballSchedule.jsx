aad1a1385467b9f13231e7702957aaaf847473ad
adriansito <adriancrosby02@gmail.com>
2026-02-11 21:41:45 -0800

    created reusable helper function to calculate sport record

--- src/components/schedulePage/BaseballSchedule.jsx
+++ src/components/schedulePage/BaseballSchedule.jsx
@@ -1,23 +1,13 @@
 import logo from '../../assets/Concrete-Logo.png';
 import SchedulePage from './SchedulePage';
 import { useSchedules } from '../../hooks/useSchedules';
+import recordKeeper from '../../utils/recordKeeper';
 import './schedulePage.css';
 
 export default function BaseballSchedule() {
 	const { games, loading } = useSchedules('baseball');
-	let wins = 0;
-	let loss = 0;
-	let record;
-	games.map((game) => {
-		console.log(game.result.includes('W'));
+	let record = recordKeeper(games);
 
-		if (game.result.includes('W')) {
-			wins += 1;
-		} else if (game.result.includes('L')) {
-			loss += 1;
-		}
-	});
-	record = `${wins} - ${loss}`;
 	if (loading) {
 		return <div>Loading baseball schedule...</div>;
 	}
