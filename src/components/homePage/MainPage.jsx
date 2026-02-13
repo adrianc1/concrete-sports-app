4040dec53c523b567b35348c17aadf8f1a68c16a
adriansito <adriancrosby02@gmail.com>
2026-02-12 20:24:53 -0800

    added schedule section to desktop mode. hidden on mobile

--- src/components/homePage/MainPage.jsx
+++ src/components/homePage/MainPage.jsx
@@ -1,12 +1,14 @@
 import '../homePage/homePageStyles/mainPage.css';
 import RecentGames from '../../layout/RecentGames.jsx';
 import UpcomingGames from './UpcomingGames.jsx';
+import ScheduleSection from '../../layout/ScheduleSection.jsx';
 
 function MainPage() {
 	return (
 		<>
 			<RecentGames />
 			<UpcomingGames />
+			<ScheduleSection />
 		</>
 	);
 }
